36ca4cdd203a3f10d6363b9c5dc98c2dae46d71d
Koin <keflok23@gmial.com>
2025-01-03 16:46:18 -0500

    Se borro lo extra y se dejo lo que se usa

--- app/src/main/java/com/example/loginconvocatoria/navigation/NavScreen.kt
+++ app/src/main/java/com/example/loginconvocatoria/navigation/NavScreen.kt
@@ -3,10 +3,8 @@ package com.example.loginconvocatoria.navigation
 enum class NavScreen(val route: String) {
     HomeScreen("home_screen"),
     LoginScreen("login_screen"),
-    Ruta1("ruta1"),
     Ruta2("ruta2"),
     DudasInformacion("Dudas_Informacion"),
-    Ruta3("ruta3"),
     PageContent("page_content"),
     CreationAgend("Creation_Agend"),
     AgendaVisua("Agenda_Visua"),
